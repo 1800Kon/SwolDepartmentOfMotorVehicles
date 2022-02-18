8edd5d708478a8c6217654c2ee625f85f1f0641e
Konstan <55129891+1800Kon@users.noreply.github.com>
2022-02-18 22:11:18 +0100

    zoom into swol + collage

--- bot.py
+++ bot.py
@@ -22,15 +22,34 @@ async def on_message(message):
         return
 
     if message.content.lower().startswith('swol'):
-        await message.channel.send('Generating your license...')
-        swolNo = message.content.lower().split()[1]
-        image_url_template = "https://www.smolverse.lol/_next/image?url=https%3A%2F%2Ftreasure-marketplace.mypinata.cloud%2Fipfs%2FQmSqwxNFMeFtgdCnjBTTixx46Wi6TH9FtQ5jAp98JnAoeR%2F" + swolNo + "%2F5.png&w=750&q=100"
-        
-        with BytesIO() as image_binary:
-            img = Image.open(requests.get(image_url_template, stream=True).raw)
-            img.save(image_binary, 'PNG')
-            image_binary.seek(0)
-            await message.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
+        if len(message.content.lower().split(" ")) == 2:
+            await message.add_reaction('✅')
+            swolNo = message.content.lower().split()[1]
+            await message.channel.send('Generating your license...')
+            image_url_template = "https://www.smolverse.lol/_next/image?url=https%3A%2F%2Ftreasure-marketplace.mypinata.cloud%2Fipfs%2FQmSqwxNFMeFtgdCnjBTTixx46Wi6TH9FtQ5jAp98JnAoeR%2F" + swolNo + "%2F5.png&w=750&q=100"
+
+            with BytesIO() as image_binary:
+                try:
+                    swol = Image.open(requests.get(
+                        image_url_template, stream=True).raw).convert("RGBA")
+                    zoomed_swol = zoom_at(swol, 200, 100, 5)
+                    bg = Image.open('images/beach.jpg').convert("RGBA")
+                    bg.paste(zoomed_swol, (0, 0), zoomed_swol)
+                    bg.save(image_binary, 'PNG')
+                    image_binary.seek(0)
+                    await message.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
+                except:
+                    await message.channel.send('Something went wrong. Please try again.')
+        else:
+            return
+
+
+def zoom_at(img, x, y, zoom):
+    w, h = img.size
+    zoom2 = zoom * 2
+    img = img.crop((x - w / zoom2, y - h / zoom2,
+                    x + w / zoom2, y + h / zoom2))
+    return img.resize((w, h), Image.LANCZOS)
 
 
 client.run(token)
